09bc30c9303a6478072a4e270e4fcbc1041e1120
Fiona <fiona_berkery@hotmail.com>
2022-05-29 15:54:25 +0100

    mvp for score completed

---
+++ client/src/components/FinishPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FinishPage = ({finalScore}) => {
+
+    return (
+
+        <>
+        <h1> RIP 🪦 </h1>
+            <h1> Final Score : {finalScore} </h1>
+        </>
+    )
+}
+
+export default FinishPage
--- client/src/components/Score.js
+++ client/src/components/Score.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 
-const Score = () => {
+const Score = ({runningScore, setRunningScore, finalScore, setFinalScore}) => {
+
+    const second = 1000
+    const [scoreRate, setScoreRate] = useState(second)
+
+    const scoreRef = useRef(runningScore)
+
+    useEffect(() => {
+        scoreRef.current = setInterval(() => {
+            setRunningScore((scoreRef.current += scoreRate))
+        }, scoreRate)
+        return () => {
+            clearInterval(scoreRef.current)
+        }
+    }, [scoreRate])
+
+    let score = Math.floor((scoreRef.current / 1000) * 5)
+    setFinalScore(score)
+    
     return (
     
-    <div>Score</div>
+            <div> Running score : {score} </div>
     
-)
+            )
     }
+
 export default Score
