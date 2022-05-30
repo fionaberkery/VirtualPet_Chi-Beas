610ed9ced89ba24bce3e2461bbe2f3d4957a5eb7
Fiona <fiona_berkery@hotmail.com>
2022-05-30 21:44:40 +0100

    FB css changes

--- client/src/components/FinishPage.js
+++ client/src/components/FinishPage.js
@@ -1,12 +1,15 @@
 import React from "react";
+import gameOver from '../images/game_over.png'
 
 const FinishPage = ({finalScore, name}) => {
 
     return (
 
         <>
-            <h1> {name} is dead </h1>
-            <h3>Final Score : {finalScore} </h3>
+            <img id="game-over-image" src={gameOver} width="250" height="250"/>
+            
+            <h1 id="header"> {name} is dead </h1>
+            <h3 id="header">Final Score : {finalScore} </h3>
         </>
     )
 }
