62d44b53eb79f26464bbeabe11610efc6f8a166d
Aaron Hubber <aaronhubber@gmail.com>
2022-05-30 11:56:59 +0100

    update for three phases complete

--- client/src/components/Monster.js
+++ client/src/components/Monster.js
@@ -1,11 +1,123 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react';
+import egg from "../images/Egg.png"
+import grave from "../images/grave.png"
+import idle3 from "../images/idle3.png"
 
-const Monster = () => {
-    
-    return (
-        
-        <div>Monster</div>
-    )
+const spriteWidth = 180
+const spriteHeight = 120
+let frameX = 0
+let frameY = 0
+let gameFrame = 0
+const staggerFrames = 15
+const staggerFramesIdle = 30
+const staggerFramesGrave = 50
+
+const Egg = props =>{
+
+    const canvasRef = useRef (null)
+
+    const eggImage = new Image()
+    eggImage.src = egg
+
+    const draw = (context) => {
+        context.clearRect (0,0,context.canvas.width, context.canvas.height)
+      // context.fillReact = (100,30,100,100)
+    context.drawImage(eggImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth,spriteHeight)
+    if (gameFrame % staggerFrames == 0){
+        if (frameX < 4) frameX++ 
+        else frameX = 0
+    }
+    gameFrame++
+    context.fill()
+    } 
+
+    useEffect (() =>{
+        const canvas = canvasRef.current
+        const context = canvas.getContext("2d")
+        let frameCount = 0
+        let animationFrameId
+        const render = () => {
+        frameCount++
+        draw(context, frameCount)
+        animationFrameId = window.requestAnimationFrame(render)
+    }
+    render()
+    return () => {
+        window.cancelAnimationFrame(animationFrameId)}
+    },[draw])
+    return <canvas ref={canvasRef} {...props}/>
 }
 
-export default Monster
+export const Idle = props =>{
+
+    const canvasRef = useRef (null)
+
+    const idleImage = new Image()
+    idleImage.src = idle3
+
+    const draw = (context) => {
+        context.clearRect (0,0,context.canvas.width, context.canvas.height)
+      // context.fillReact = (100,30,100,100)
+    context.drawImage(idleImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth,spriteHeight)
+    if (gameFrame % staggerFramesIdle == 0){
+        if (frameX < 9) frameX++ 
+        else frameX = 0
+    }
+    gameFrame++
+    context.fill()
+    } 
+
+    useEffect (() =>{
+        const canvas = canvasRef.current
+        const context = canvas.getContext("2d")
+        let frameCount = 0
+        let animationFrameId
+        const render = () => {
+        frameCount++
+        draw(context, frameCount)
+        animationFrameId = window.requestAnimationFrame(render)
+    }
+    render()
+    return () => {
+        window.cancelAnimationFrame(animationFrameId)}
+    },[draw])
+    return <canvas ref={canvasRef} {...props}/>
+}
+
+export const Grave = props =>{
+
+    const canvasRef = useRef (null)
+
+    const graveImage = new Image()
+    graveImage.src = grave
+
+    const draw = (context) => {
+        context.clearRect (0,0,context.canvas.width, context.canvas.height)
+      // context.fillReact = (100,30,100,100)
+    context.drawImage(graveImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth,spriteHeight)
+    if (gameFrame % staggerFramesGrave == 0){
+        if (frameX < 4) frameX++ 
+        else frameX = 0
+    }
+    gameFrame++
+    context.fill()
+    } 
+
+    useEffect (() =>{
+        const canvas = canvasRef.current
+        const context = canvas.getContext("2d")
+        let frameCount = 0
+        let animationFrameId
+        const render = () => {
+        frameCount++
+        draw(context, frameCount)
+        animationFrameId = window.requestAnimationFrame(render)
+    }
+    render()
+    return () => {
+        window.cancelAnimationFrame(animationFrameId)}
+    },[draw])
+    return <canvas ref={canvasRef} {...props}/>
+}
+
+export default Egg;
--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -1,54 +1,14 @@
 import '../App.css';
 import React, {useState, useEffect, useRef} from 'react';
-import Header from '../components/Header.js'
 import Score from '../components/Score';
 import Timer from '../components/Timer';
 import FinishPage from '../components/FinishPage';
-import egg from "../images/Egg.png"
-const spriteWidth = 180
-const spriteHeight = 120
-let frameX = 0
-let frameY = 0
-let gameFrame = 0
-const staggerFrames = 15
-const TIME_LIMIT = 5000;
+import Egg from '../components/Monster';
+import { Idle } from '../components/Monster';
+import { Grave } from '../components/Monster';
 
+const TIME_LIMIT = 15000;
 
-const Canvas = props =>{
-
-  const canvasRef = useRef (null)
-
-  const eggImage = new Image()
-  eggImage.src = egg
-
-  const draw = (context, frameCount) => {
-    context.clearRect (0,0,context.canvas.width, context.canvas.height)
-    // context.fillReact = (100,30,100,100)
-    context.drawImage(eggImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth,spriteHeight)
-    if (gameFrame % staggerFrames == 0){
-      if (frameX < 4) frameX++ 
-      else frameX = 0
-    }
-    gameFrame++
-    context.fill()
-  } 
-
-  useEffect (() =>{
-    const canvas = canvasRef.current
-    const context = canvas.getContext("2d")
-    let frameCount = 0
-    let animationFrameId
-    const render = () => {
-      frameCount++
-      draw(context, frameCount)
-      animationFrameId = window.requestAnimationFrame(render)
-    }
-    render()
-    return () => {
-      window.cancelAnimationFrame(animationFrameId)}
-    },[draw])
-  return <canvas ref={canvasRef} {...props}/>
-}
 
 const  Game = (()=> {
   
@@ -80,6 +40,7 @@ console.log(finalScore)
       <>
         <h1>Start Game</h1> 
         <button onClick={startGame}>Start the Game!</button>
+        <Egg id="canvas"></Egg>
       </>}
     
     {playing && !finished &&
@@ -88,7 +49,7 @@ console.log(finalScore)
         <Timer time={TIME_LIMIT} onEnd={endGame}/>
         <Score tempFinalScore={tempFinalScore} setFinalScore={setFinalScore} runningScore={runningScore} setRunningScore={setRunningScore} />
         <br></br>
-        <Canvas id="canvas"></Canvas>
+        <Idle id="canvas"></Idle>
       </>}
     
 
@@ -96,6 +57,7 @@ console.log(finalScore)
       <>
         
         <FinishPage finalScore={finalScore}/> 
+        <Grave id="canvas"></Grave>
       </>}
     </>
   )
