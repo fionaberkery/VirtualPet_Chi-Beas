799df0ed17253ef286c0ccb38a6b5ae0d606f6bb
Fiona <fiona_berkery@hotmail.com>
2022-05-31 09:27:06 +0100

    css changes to font and buttons

--- client/src/components/FinishPage.js
+++ client/src/components/FinishPage.js
@@ -1,13 +1,11 @@
 import React from "react";
-import gameOver from '../images/game_over.png'
+
 
 const FinishPage = ({finalScore, name}) => {
 
     return (
 
         <>
-            <img id="game-over-image" src={gameOver} width="250" height="250"/>
-            
             <h1 id="header"> {name} is dead </h1>
             <h3 id="header">Final Score : {finalScore} </h3>
         </>
